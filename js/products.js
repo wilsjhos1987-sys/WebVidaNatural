e82fa6ca5119d20f72e0fc0817fa02742970fe2d
wilsjhos1987-sys <wils.jhos1987@gmail.com>
2026-01-16 00:48:53 -0500

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,62 +1,4 @@
-// Datos de productos
-/** const products = [
-    {
-        id: 1,
-        name: "Quemador Natural Premium",
-        category: "control-peso",
-        price: 32.99,
-        image: "https://www.nutrimind.net/images/news/analisis_quemadores_grasa/2.png",
-        description: "Ayuda natural para acelerar el metabolismo y controlar el peso.",
-        badges: ["natural", "vegan"]
-    },
-    {
-        id: 2,
-        name: "Crema Facial de Árgan",
-        category: "piel",
-        price: 28.50,
-        image: "https://images.pexels.com/photos/6663374/pexels-photo-6663374.jpeg?_gl=1*17vko3z*_ga*NTI4MzE5MDcxLjE3NjgzNjYxMTc.*_ga_8JE65Q40S6*czE3NjgzNjYxMTckbzEkZzEkdDE3NjgzNjc4MjUkajU1JGwwJGgw",
-        description: "Hidratación profunda con aceite de argán orgánico.",
-        badges: ["organic", "vegan"]
-    },
-    {
-        id: 3,
-        name: "Aceite Esencial Lavanda",
-        category: "antiestres",
-        price: 18.75,
-        image: "https://images.pexels.com/photos/17466173/pexels-photo-17466173.jpeg?_gl=1*4w0258*_ga*NTI4MzE5MDcxLjE3NjgzNjYxMTc.*_ga_8JE65Q40S6*czE3NjgzNjYxMTckbzEkZzEkdDE3NjgzNjc3MzUkajU5JGwwJGgw",
-        description: "Calma la mente y promueve un sueño reparador.",
-        badges: ["organic"]
-    },
-    {
-        id: 4,
-        name: "Suplemento Omega-3 Algas",
-        category: "suplementos",
-        price: 42.25,
-        image: "https://cloudinary.images-iherb.com/image/upload/f_auto,q_auto:eco/images/flo/flo61424/l/8.jpg",
-        description: "Ácidos grasos esenciales de origen vegetal.",
-        badges: ["vegan"]
-    },
-    {
-        id: 5,
-        name: "Té Verde Orgánico",
-        category: "control-peso",
-        price: 15.99,
-        image: "https://images.pexels.com/photos/5946639/pexels-photo-5946639.jpeg?_gl=1*10fgp02*_ga*NTI4MzE5MDcxLjE3NjgzNjYxMTc.*_ga_8JE65Q40S6*czE3NjgzNjYxMTckbzEkZzEkdDE3NjgzNjc2ODAkajQ3JGwwJGgw",
-        description: "Antioxidantes naturales para complementar tu dieta.",
-        badges: ["organic", "natural"]
-    },
-    {
-        id: 6,
-        name: "Jabón de Caléndula",
-        category: "piel",
-        price: 12.50,
-        image: "https://images.pexels.com/photos/7500305/pexels-photo-7500305.jpeg?_gl=1*1jyyerm*_ga*NTI4MzE5MDcxLjE3NjgzNjYxMTc.*_ga_8JE65Q40S6*czE3NjgzNjYxMTckbzEkZzEkdDE3NjgzNjgyNTgkajU5JGwwJGgw",
-        description: "Suave limpieza para pieles sensibles.",
-        badges: ["organic", "natural", "vegan"]
-    }
-];
-*//
-/* 1. Cargar productos desde el servlet */
+
 const API = 'http://localhost:8080/VidaNaturalAPI/api/productos';
 
 let products = [];   // lo llenaremos con el fetch
@@ -149,5 +91,6 @@ if (typeof module !== 'undefined' && module.exports) {
     module.exports = { products, renderProducts, getCategoryName };
 
 }
+
 
 
